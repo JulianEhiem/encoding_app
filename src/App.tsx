fc90e271d97dbb248f87561b26884cca860483c5
JulianEhiem <jules.ehiem@gmail.com>
2023-12-22 10:14:06 -0500

    Setting up and connecting apolloclient to apolloserver

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { useQuery } from '@apollo/client';
+import {GET_ATHLETES} from "./schemas/queries/getAthletes";
+
+interface Athlete {
+    name: string;
+    sex: string;
+    specialty: string;
+}
 
 function App() {
+    const [athlete, setAthlete] = useState<Athlete | null>(null)
+    const { loading, error } = useQuery(GET_ATHLETES, {onCompleted: (data) => {
+            setAthlete(data.athletes[0])
+        }});
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="underline">Welcome to encoding app</h1>
-      </header>
-    </div>
+          <div className="App">
+              <header className="App-header">
+                  <h1 className="underline">Welcome to encoding app {athlete?.name || ''}</h1>
+              </header>
+          </div>
+
   );
 }
 
